6749f0d0d302427b9910fa26f9aec9f1bcb9df7f
Артём <71376415+artemik2ww@users.noreply.github.com>
2026-05-18 15:30:19 +0300

    b125

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 // Copyright 2021 NNTU-CS
+#include <string>
 #include "bst.h"
+
 void makeTree(BST<std::string>& tree, const char* filename);
 void printFreq(BST<std::string>& tree);
-
 int main() {
     BST<std::string> tree;
     makeTree(tree, "src/war_peace.txt");
